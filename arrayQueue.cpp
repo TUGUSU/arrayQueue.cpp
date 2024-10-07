6fb18f925e909495c625cf77e6746c7a1a50ae4c
TUGUSU_G <163750263+TUGUSU@users.noreply.github.com>
2024-10-07 20:37:41 +0800

    Update arrayQueue.cpp

--- arrayQueue.cpp
+++ arrayQueue.cpp
@@ -3,31 +3,34 @@
 
 using namespace std;
 
+// Print all elements in the queue
 void printqueue(ArrayQueue &qq) {
-  cout << "[ ";
-  for (int i = 0; i < qq.size; i++) {
-    cout << qq.values[i] << " ";
-  }
-  cout << "] L:" << qq.size << endl;
+    cout << "[ ";
+    for (int i = 0; i < qq.size; i++) {
+        cout << qq.values[(qq.front + i) % qq.capacity] << " ";  // Adjusted to print from the front
+    }
+    cout << "] L:" << qq.size << endl;
 }
 
+// Add an element to the queue
 void enqueue(ArrayQueue &qq, int n) {
     if (qq.size == qq.capacity) {
         cout << "Queue is full!" << endl;
         return;
     }
-    qq.rear = (qq.rear + 1) % qq.capacity; // dugui helberer osgoh
-    qq.values[qq.rear] = n;  // shine element nemh
-    qq.size++;  // hemjeeg ihesgeh
+    qq.rear = (qq.rear + 1) % qq.capacity;  // Circular increment for rear
+    qq.values[qq.rear] = n;                 // Add new element
+    qq.size++;                              // Increase size
 }
 
+// Remove an element from the queue
 int dequeue(ArrayQueue &qq) {
     if (qq.size == 0) {
         cout << "Queue is empty!" << endl;
         return -1;
     }
-    int removedValue = qq.values[qq.front];  // ehnii elementin utgiig avah
-    qq.front = (qq.front + 1) % qq.capacity; // dugui helberer ihesgeh
-    qq.size--;  // hemjeeg bagasgah
+    int removedValue = qq.values[qq.front];     // Get the front element
+    qq.front = (qq.front + 1) % qq.capacity;    // Circular increment for front
+    qq.size--;                                  // Decrease size
     return removedValue;
 }
